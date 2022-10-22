5be2750f7181948860f65ceea7f4966def954f81
AndrMenezes <andrefelipemaringa@gmail.com>
2022-10-22 17:37:43 +0100

    :bug: fixing names of .qmd files

--- setup.R
+++ setup.R
@@ -2,4 +2,4 @@ renv::restore()
 files <- c("index.qmd",
            "./processing/processing_steps.qmd",
            "./downstream_analysis/differential expression.qmd")
-for (f in files) quarto::quarto_render(input = "./downstream_analysis/differential expression.qmd")
+for (f in files) quarto::quarto_render(input = f)
