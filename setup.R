dca4da6d201e33f8644cbbcdf365e1a3b3c3d90d
AndrMenezes <andrefelipemaringa@gmail.com>
2022-11-02 15:12:52 +0000

    :rocket: update reports html

--- setup.R
+++ setup.R
@@ -1,5 +1,6 @@
 renv::restore()
 files <- c("index.qmd",
            "./processing/processing_steps.qmd",
-           "./downstream_analysis/differential expression.qmd")
+           "./downstream_analysis/feature_selection.qmd",
+           "./downstream_analysis/differential_expression.qmd")
 for (f in files) quarto::quarto_render(input = f)
