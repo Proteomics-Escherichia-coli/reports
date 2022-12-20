c81e769a55048b2314ee32bc3441950a9bc12d40
AndrMenezes <andrefelipemaringa@gmail.com>
2022-12-20 17:36:24 +0000

    updating

--- setup.R
+++ setup.R
@@ -2,5 +2,5 @@ renv::restore()
 files <- c("index.qmd",
            "./processing/processing_steps.qmd",
            "./downstream_analysis/feature_selection.qmd",
-           "./downstream_analysis/differential_expression.qmd")
+           "./downstream_analysis/protein_abundance.qmd")
 for (f in files) quarto::quarto_render(input = f)
